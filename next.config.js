901625466b2284408b4dae18b3edfb5a4ca9d303
austrokhart <austrokhart@gmail.com>
2021-02-25 14:08:20 +0300

    update file structure

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
-const nextTranslate = require('next-translate')
+const withPlugins = require('next-compose-plugins');
+const translate = require('next-translate')
+const optimizedImages = require('next-optimized-images')
+const analyzer = require('@next/bundle-analyzer')
 
-module.exports = nextTranslate({})
+module.exports = withPlugins([optimizedImages, translate, analyzer]);
